db68f9ccd0727e294a169cea6c0dfaf2732e56b2
S.W. Smith <spencerwayne310@gmail.com>
2024-05-10 17:31:04 -0300

    use api to generate random cat facts

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const footer = document.getElementById('footer-text');
 const date = new Date();
 footer.innerHTML = `Copyright &copy; ${date.getFullYear()} S.W. Smith`;
+
+const test_api = "https://catfact.ninja/fact";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await fetch(test_api, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json()).then(res => {
+        console.log("RES: ", res);
+        const test = document.getElementById("test");
+        test.innerText = res.fact;
+    }).catch(err => console.error("ERROR: ", err));
+})
